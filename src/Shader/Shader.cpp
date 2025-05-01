f1f7f29f2dbe5213406a3d9eebffb1235402b38c
MaxVarverakis <77653658+MaxVarverakis@users.noreply.github.com>
2025-05-01 15:07:03 -0700

    Added uniform2f helper

--- src/Shader/Shader.cpp
+++ src/Shader/Shader.cpp
@@ -83,6 +83,11 @@ void Shader::unbind() const
     GLCall(glUseProgram(0));
 }
 
+void Shader::setUniform2f(const std::string& name, float v0, float v1)
+{
+    GLCall(glUniform2f(getUniformLocation(name), v0, v1));
+}
+
 void Shader::setUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
 {
     GLCall(glUniform4f(getUniformLocation(name), v0, v1, v2, v3));
--- src/Shader/Shader.hpp
+++ src/Shader/Shader.hpp
@@ -36,6 +36,8 @@ public:
 
     void unbind() const;
     
+    void setUniform2f(const std::string& name, float v0, float v1);
+
     void setUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
     void setUniform4f(const std::string& name, float val);
